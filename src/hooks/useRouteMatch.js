a37acf687e8ef7f0664eaae9050c986cf899ae85
LKB189 <117663426+lavlesh20214262671@users.noreply.github.com>
2024-08-21 22:24:13 +0530

    Update useRouteMatch.js

--- src/hooks/useRouteMatch.js
+++ src/hooks/useRouteMatch.js
@@ -3,4 +3,6 @@ import { useLocation, matchPath } from "react-router-dom";
 export default function useRouteMatch(path) {
   const location = useLocation();
   return matchPath(location.pathname, { path });
+
+  //
 }
